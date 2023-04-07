70f980ff1bcfdb29589159c85e22e1b134ff7cfd
Simon Lightfoot <simon@devangels.london>
2023-04-07 22:13:02 +0100

    Add PageListPerformanceOptimizer

--- lib/page_list_viewport.dart
+++ lib/page_list_viewport.dart
@@ -1,6 +1,7 @@
 library page_list_viewport;
 
 export 'src/page_list_viewport.dart';
+export 'src/page_list_performance_optimizer.dart';
 export 'src/logging.dart';
 
 export 'src/deprecated_gestures.dart';
